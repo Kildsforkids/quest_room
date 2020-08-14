c59937034bb0806e9431f8a306c3c1715380e7b5
Igor Silchenko <kildsforkids@gmail.com>
2020-08-14 17:55:00 +1000

    Создана сцена для тестирования Ragdoll физики рук

---
+++ Assets/Scripts/RagdollHandSnap.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RagdollHandSnap : MonoBehaviour
+{
+    [SerializeField]
+    private Transform hand;
+
+    private void LateUpdate()
+    {
+        transform.localPosition = hand.localPosition;
+        transform.localRotation = hand.localRotation;
+        transform.Rotate(Vector3.up, 180f, Space.Self);
+    }
+}
