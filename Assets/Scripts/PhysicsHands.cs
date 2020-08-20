046f2b627dcd102a38869f4efa520cc1d5bbe477
Igor Silchenko <kildsforkids@gmail.com>
2020-08-20 17:58:10 +1000

    Добавление ragdoll'а к пальцам левой руки

---
+++ Assets/Scripts/PhysicsHands.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PhysicsHands : MonoBehaviour
+{
+    [SerializeField]
+    private Transform handTarget;
+    [SerializeField]
+    private float forceAmount;
+
+    public Vector3 Pos { get; set; }
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var dir = handTarget.position - transform.position;
+        Pos = dir.normalized * dir.magnitude * forceAmount;
+
+        rb.AddForce(Pos);
+    }
+}
