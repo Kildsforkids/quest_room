25fe7f363ac185bf609971b5b22892a8e547a21a
Igor Silchenko <kildsforkids@gmail.com>
2020-08-21 17:55:18 +1000

    Тест разных способов реализации физики рук

---
+++ Assets/Scripts/SnapTransform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SnapTransform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private bool rotate = false;
+
+    private void Update()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+
+        if (rotate)
+            transform.Rotate(Vector3.up, 180f, Space.Self);
+    }
+}
